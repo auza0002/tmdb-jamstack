d6eca0af277e3fb212529fd2f1e54a9590566ad9
Diego Auza <auza0002@algonquinlive.com>
2022-12-08 00:21:29 -0500

    nav css

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const APP = {
   selectCategories: "",
   inputvalue: "",
   ul: document.querySelector("#ulInfo"),
+  containerDiv: document.querySelector("#containerDiv"),
   df: new DocumentFragment(),
   init: {
     movie: document.querySelector("#movie"),
@@ -71,125 +72,96 @@ const APP = {
   creatInnerHtmlMovie: function (obj) {
     console.log(obj);
     console.log(obj["results"]);
-    console.log(obj["results"][0].poster_path);
-    obj["results"].forEach((item) => {
+    let newResult = obj["results"].slice(1, obj["results"].length);
+    containerDiv.innerHTML = `
+    <div class="banner_img">
+      <div>
+        <img src='http://image.tmdb.org/t/p/w500/${obj["results"][0].poster_path}'>
+      </div>
+    <div>    
+        <h3>${obj["results"][0].original_name}</h3>
+        <p>${obj["results"][0].overview}</p>
+        </div>
+    </div>
+    `;
+    newResult.forEach((item) => {
       const li = document.createElement("li");
       if (item.poster_path === null) {
         li.innerHTML = `
-        <div>
-        <img src='${APP.CatImg}'>
-        <h2>${item.original_name}</h2>
-        <p>${item.overview}</p>
+        <div class="thecard">
+          <div class="thefront">
+            <img src='${APP.CatImg}'>
+          </div>
+          <div class="theback">
+            <h3>${item.original_name}</h3>
+            <p>${item.overview}</p>
+          </div>
         </div>
         `;
       } else {
         li.innerHTML = `
         <div>
         <img src='http://image.tmdb.org/t/p/w500/${item.poster_path}'>
-        <h2>${item.original_name}</h2>
+        <h3>${item.original_name}</h3>
         <p>${item.overview}</p>
         </div>
         `;
       }
+      APP.bannerImg(obj);
       APP["df"].append(li);
     });
   },
   creatInnerHtmlTv: function (obj) {
     console.log(obj);
     console.log(obj["results"]);
-    console.log(obj["results"][0].poster_path);
-    obj["results"].forEach((item) => {
+    let newResult = obj["results"].slice(1, obj["results"].length);
+    containerDiv.innerHTML = `
+    <div class="banner_img">
+        <div><img class="img_split" src='http://image.tmdb.org/t/p/w500/${obj["results"][0].poster_path}'></div>
+
+    <div>    
+        <h3>${obj["results"][0].original_title}</h3>
+        <p>${obj["results"][0].overview}</p>
+        </div>
+    </div>
+    `;
+    newResult.forEach((item) => {
       const li = document.createElement("li");
       if (item.poster_path === null) {
         li.innerHTML = `
-        <div>
-        <img src='${APP.CatImg}'>
-        <h2>${item.original_title}</h2>
-        <p>${item.overview}</p>
+        <div class="thecard">
+          <div class="thefront">
+            <img src='${APP.CatImg}'>
+          </div>
+        <div class="theback">
+          <h3>${item.original_title}</h3>
+          <p>${item.overview}</p></div>
         </div>
         `;
       } else {
         li.innerHTML = `
-        <div>
-        <img src='http://image.tmdb.org/t/p/w500/${item.poster_path}'>
-        <h2>${item.original_title}</h2>
-        <p>${item.overview}</p>
+        <div class="thecard">
+          <div class="thefront">
+            <img src='http://image.tmdb.org/t/p/w500/${item.poster_path}'>
+          </div>
+        <div class="theback">
+          <h3>${item.original_title}</h3>
+          <p>${item.overview}</p></div>
         </div>
         `;
       }
+      APP.bannerImg(obj);
       APP["df"].append(li);
     });
+  },
+  bannerImg: function (obj) {
+    let banner = document.querySelector(".banner");
+    banner.setAttribute(
+      `style`,
+      `background-image: url(http://image.tmdb.org/t/p/original/${obj["results"][0].backdrop_path});background-repeat:no-repeat;background-size: cover;`
+    );
   },
 };
 
 document.addEventListener("DOMContentLoaded", APP.init.main);
 console.log(APP.init);
-
-// function main() {
-//   // !VARIABLES
-//   const movies = document.querySelector("#movies");
-//   const tvShows = document.querySelector("#tvShows");
-//   const btn = document.querySelector("#searchsubmit");
-//   const key = "d1a21373ff4a38d759b3e1fdd4b4c09f";
-//   let ul = document.querySelector("#ulInfo");
-//   let selectCategories = "";
-//   let inputvalue = "";
-//   movies.addEventListener("click", checkev);
-//   tvShows.addEventListener("click", checkev);
-//   btn.addEventListener("click", getInputValue);
-
-//   // ! -----CHECK CATEGORIES------
-//   function checkev(ev) {
-//     let categories = ev.target.getAttribute("value");
-//     if (categories === movies) {
-//       console(categories);
-//       selectCategories = categories;
-//     } else {
-//       selectCategories = categories;
-//     }
-//     console.log(selectCategories, "holaaa si sirvo");
-//   }
-//   // ? --------END CHECK CATEGORIES-----
-//   // ! -------GET INPUT VAL-----
-//   function getInputValue() {
-//     let inputElement = document.getElementById("keysssb").value.trim();
-//     if (inputElement === "") {
-//       console.log("error");
-//     } else {
-//       inputvalue = inputElement;
-//       console.log(inputvalue);
-//       getData();
-//     }
-//   }
-//   // ?  ------END INPUT VAL------
-//   // ! -----FECTH DATA -----
-//   function getData() {
-//     if (selectCategories && inputvalue) {
-//       ul.innerHTML = ``;
-//       let url = `https://api.themoviedb.org/3/search/${selectCategories}?query=${inputvalue}&api_key=${key}`;
-//       console.log(url);
-//       fetch(url)
-//         .then((response) => {
-//           if (!response.ok) {
-//             console.log("no response");
-//           }
-//           return response;
-//         })
-//         .then((response) => response.json())
-//         .then((obj) => {
-//           creatInnerHtml(obj);
-//         });
-//     }
-//   }
-//   // ? -----END FECTH DATA -----
-//   // !-------CREAT INNET HTML
-//   function creatInnerHtml(obj) {
-//     obj["results"].forEach((element) => {
-//       console.log(element.original_name);
-//     });
-//   }
-//   // ?------- END CREAT INNET HTML----
-//   // ! ------ END MAIN -------
-// }
-
-// `https://api.themoviedb.org/3/search/${ev.target.value}?query=&api_key=e3cb87119b43a590d35eb
